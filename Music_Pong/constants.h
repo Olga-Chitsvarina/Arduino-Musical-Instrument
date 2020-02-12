501e9cdba0670c20f77f92cc2daf07da63ff5803
Jay Montal <Jaedwin.Montal@gmail.com>
2020-02-11 19:09:12 -0700

    Add Music_Pong Arduino Files
    All required files for Music_Pong

---
+++ Music_Pong/constants.h
@@ -0,0 +1,14 @@
+#define VOLUME 127
+#define MAX_INDEX 5 // TO-DO: Need to use this or remove this
+
+// Number of the LDR pins
+#define LDR_1_PIN 15  
+#define LDR_2_PIN 14
+#define LDR_3_PIN 13
+#define LDR_4_PIN 12
+#define LDR_5_PIN 11
+#define LDR_6_PIN 10
+
+// Analog joystick pin connected to X and Y output
+#define X_PIN 0 
+#define Y_PIN 1 
---
+++ Music_Pong/notes.h
@@ -0,0 +1,8 @@
+#define NOTE_C1 24
+#define NOTE_D1 26
+#define NOTE_E1 28
+#define NOTE_F1 29
+#define NOTE_G1 31
+#define NOTE_A1 33
+#define NOTE_B1 35
+#define NOTE_C2 36
